3c9e93c83348a0c4b7501ac4c2eabe246e99df84
Rimma <s21rimma@mail.ru>
2022-04-19 21:16:56 +0300

    added papki

---
+++ seminar_2/Program.cs
@@ -0,0 +1,94 @@
+﻿
+void ShowNumber()
+{
+    int num = new Random().Next(10,99); 
+    int firstNum = num/10;
+    int secondNum = num%10;
+
+    Console.Write(num +" -> ");
+
+    if(firstNum > secondNum)
+    {
+        Console.WriteLine(firstNum);
+    }
+    else
+    {
+        Console.WriteLine(secondNum);
+    }
+}
+ShowNumber();
+/*
+// задача 1
+
+void NewNumber()
+{
+    int num = new Random().Next(100,999);
+    int firstNum = num/100;
+    int threeNum = num%10;
+    int newN = firstNum*10 + threeNum;
+    Console.WriteLine(num +" -> " + newN);
+}
+NewNumber();
+
+
+//задача 2
+
+void delenie(int num1, int num2)
+{
+    int ost = num1%num2;
+    if(ost == 0)
+    {
+      Console.WriteLine(num1 + " кратно " + num2);
+    }
+    else
+    {
+        Console.WriteLine(num1+ " не кратно " + num2 + ". Остаток = " + ost);
+    }
+}
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+delenie(num1, num2);
+
+
+// задача3
+
+void delenie(int num)
+{
+    int del7 = num%7;
+    int del23 = num%23;
+    if (del7 == 0 && del23 == 0)
+    {
+        Console.WriteLine(num +" делиться на 7 и 23 нацело");
+    }
+    else
+    {
+        Console.WriteLine(num +" не делиться ");
+    }
+}
+Console.WriteLine("введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+delenie(num);
+
+
+// задача 4
+
+void kvadrat(int num, int kvadr)
+{
+    if(kvadr == num*num)
+    {
+        Console.WriteLine(kvadr + " является квадратом числа " + num);
+    }
+    else
+    {
+        Console.WriteLine(kvadr + " НЕ является квадратом числа " + num);
+    }
+}
+Console.WriteLine("Ввудите число возводимое в квадрат: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите предполагаемый квадрат первого числа: ");
+int kvadr = Convert.ToInt32(Console.ReadLine());
+kvadrat(num, kvadr);
+
+*/
---
+++ seminar_2/dz_2/Program.cs
@@ -0,0 +1,57 @@
+﻿/*
+// задача 10
+
+void secondNum(int num)
+{
+    int num2 = num/10;
+    int second = num2%10;
+    Console.WriteLine("Вторая цифра - " + second);
+}
+Console.WriteLine("Введите исходное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+secondNum(num);
+
+// задача 13
+
+void threeNum(int num)
+{
+    if(num/100 < 1)
+    {
+        Console.WriteLine("третей цифры нет");
+    }
+    else
+    {
+        while (num/100 > 10)
+            num = num/10;
+        int three = num%10;
+        Console.WriteLine("Третья цифра равна "+ three);
+    }
+}
+Console.WriteLine("Введите исходное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+threeNum(num);
+
+*/
+// задача 15
+
+void weekend(int day)
+{
+    if(day > 7)
+    {
+        Console.WriteLine("Дня недели с таким номером не существует");
+    }
+    else
+    {
+        if(day < 6)
+        {
+            Console.WriteLine("нет");
+        }
+        else
+        {
+            Console.WriteLine("да");
+        }
+    }
+}
+Console.WriteLine("введите номер дня недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+weekend(day);
