499845af9ca7c931f7c0280abcb3408adb637640
Rimma <s21rimma@mail.ru>
2022-04-27 23:54:38 +0300

    added first exemple

--- seminar_3/dz_3/Program.cs
+++ seminar_3/dz_3/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿/*
+// проверить 5 значне число, палиндром или нет
+void palindrom(int pal)
+{
+    int pal1 = pal/10000;
+    int pal5 = pal%10;
+    int pal4 = pal/10%10;
+    int pal2 = pal/1000%10;
+    if(pal1 == pal5 && pal2 ==pal4)
+        Console.WriteLine("это число - палиндром");
+    else
+        Console.WriteLine("это число не является палиндромом");
+}
+Console.WriteLine("введите 5-ти значное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+palindrom(num);
+*/
